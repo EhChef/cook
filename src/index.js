cb645334d7a48b3feaf23a5a4ce88ea67e69dea2
PREVOST Nathan <nathanp33@laposte.net>
2018-12-13 17:46:57 +0100

    feat: add fontawesome and moment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
